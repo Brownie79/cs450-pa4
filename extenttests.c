aae40ab27629225169fac2945b99aeeb4a7a08b8
spaceman <dev@blockchainspaceman.com>
2017-11-29 19:04:47 -0600

    changed tests file

--- extenttests.c
+++ extenttests.c
@@ -1,64 +1,80 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
 #include "fcntl.h"
 
-#define assert(x) if (x) { /* pass */ } else { \
-   printf(1, "assert failed %s %s %d\n", #x , __FILE__, __LINE__); \
-   exit(); \
-   }
+void printStat(struct stat *statastic);
+void makeExtent(char* path);
+void testlseek(char* path);
 
-char * testname = "extent";
-
-char buf[512];
-
-int
-main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-   int status;
+	if(argc == 1)
+	{
+		makeExtent("test_extent1");
+		//testlseek("a_l_seeked_file");
+		exit();
+	}else if(argc == 2){
+		int fd = open(argv[1], O_RDONLY);
+		struct stat statastic;
+		fstat(fd, &statastic);
+		printStat(&statastic);
+		
+	}
 
-   memset(buf, 0, 512);
+	exit();
+}
 
-   int fd;
+void testlseek(char* path)
+{
+	int fd = open(path, O_CREATE | O_EXTENT);
+	char firstString[] = "Here's something cool followed by some space";
+	write(fd, firstString, sizeof(firstString));
+	lseek(fd, 200);
+	char secondString[] = "I hope something was behind me.";
+	write(fd, secondString, sizeof(secondString));
+	close(fd);
+}
 
-   unlink("testout");
-   fd = open("testout", O_WRONLY | O_CREATE | O_EXTENT);
-   assert(fd != -1);
+void makeExtent(char* path)
+{
+	int fd = open(path, O_RDWR | O_CREATE | O_EXTENT);
+	char bigAssString[] = "This is a big string that just goes on and on and on aren't you jealous that you can't make it any bigger than this  massive string. I could if I had more time but I don't so instead I'll just ramble on and on. Are you even still reading this? Dont you have better things to do Please don't mark us off for this massive string.";
+	write(fd, bigAssString, sizeof(bigAssString));
+	close(fd);
+}
 
-   int i;
-   for (i = 0; i < 10; i++) {
-      buf[0] = i;
-      status = write(fd, buf, 512);
-         assert(status != -1);
-   }
-   status = close(fd);
-   assert(status != -1);
+void printStat(struct stat *statastic)
+{
+	printf(1, "Type: %d\niNode Number: %d\nNumber of Links: %d\nSize: %d bytes\n", statastic->type, statastic->ino, statastic->nlink, statastic->size);
+	if(statastic->type == T_EXTENT)
+	{
+		int n = 0;
+		while(statastic->addrs[n] && n < 13)
+		{
+			printf(1, "\tAddress: %x\n\t\tSize: %d blocks\n", getPtr(statastic->addrs[n]), getSize(statastic->addrs[n]));
+			++n;
+		}
+	}
+}
 
+uint
+toAddr(uint pointer, uint size)
+{
+  return ((pointer << 8) | size);
+}
 
-   int pid = fork();
-   if (pid == 0) {
-      printf(1, "Output from stat:\n");
-      char * args[] = {"stat", "testout", NULL};
-      exec(args[0], args);
-      exit();
-   } else {
-      wait();
-      printf(1, "\n");
-   }
+uint
+getPtr(uint addr)
+{
+  printf(1, "Getting ptr\n");
+  return ((addr & ~0xff) >> 8);
+}
 
-   fd = open("testout", O_RDONLY);
-   assert(fd != -1);
-
-   for (i = 0; i < 10; i++) {
-      status = read(fd, buf, 512);
-      assert(status != -1);
-      printf(1, "Wanted Buffer: %d\n", i);
-      printf(1, "Actual Buffer: %d\n", buf[0]);
-      assert(buf[0] == i);
-   }
-   status = close(fd);
-   assert(status != -1);
-
-   printf(1, "%s TEST PASSED\n", testname);
-   exit();
+uint
+getSize(uint addr)
+{
+  printf(1, "Getting size\n");
+  return (addr & 0xff);
 }
