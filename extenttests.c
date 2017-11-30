a2f05c1dcd1178694363904b9b42a015d564811a
Hayoung Lim <hlim17@hawk.iit.edu>
2017-11-29 23:59:52 -0600

    testing

--- extenttests.c
+++ extenttests.c
@@ -4,77 +4,123 @@
 #include "fs.h"
 #include "fcntl.h"
 
-void printStat(struct stat *statastic);
-void makeExtent(char* path);
-void testlseek(char* path);
-
-int main(int argc, char* argv[])
+void printStat(struct stat *st)
 {
-	if(argc == 1)
-	{
-		makeExtent("test_extent1");
-		//testlseek("a_l_seeked_file");
-		exit();
-	}else if(argc == 2){
-		int fd = open(argv[1], O_RDONLY);
-		struct stat statastic;
-		fstat(fd, &statastic);
-		printStat(&statastic);
-		
-	}
+  //print stat info
+  char *filetype = "";
+  if(st->type == T_EXTENT){
+    filetype = "extent-based file";
+  }else{
+    filetype = "pointer-based file";
+  }
 
-	exit();
+  printf(1, "Type: %s\n", filetype);
+  printf(1, "Device: %d\n", st->dev);
+  printf(1, "Inode #: %d\n", st->ino);
+  printf(1, "Number of links: %d\n", st->nlink);
+  printf(1, "Size: %d\n", st->size);
+
+  //extent-based file
+  if(st->type == T_EXTENT) {
+    int i=0;
+    while(st->addrs[i] && i < NDIRECT+1){
+      //first 3 byte is pointer and remaining 1 byte is length
+      printf(1, "Pointer: %x\n", ((st->addrs[i] & ~0xff) >> 8));
+      printf(1, "Length: %d\n", (st->addrs[i] & 0xff));
+      ++i;
+    }
+  }
 }
 
-void testlseek(char* path)
+int main(int argc, char *argv[])
 {
-	int fd = open(path, O_CREATE | O_EXTENT);
-	char firstString[] = "Here's something cool followed by some space";
-	write(fd, firstString, sizeof(firstString));
-	lseek(fd, 200);
-	char secondString[] = "I hope something was behind me.";
-	write(fd, secondString, sizeof(secondString));
-	close(fd);
+
+  //create a new test file
+  int fd1 = open("extent_test1", O_RDWR | O_CREATE | O_EXTENT);
+  int fd2 = open("extent_test2", O_RDWR | O_CREATE | O_EXTENT);
+  int fd3 = open("extent_test3", O_RDWR | O_CREATE | O_EXTENT);
+  int fd4 = open("extent_test4", O_RDWR | O_CREATE | O_EXTENT);
+  
+  int regularFd1 = open("extent_test_reg1", O_CREATE | O_RDWR);
+  int regularFd2 = open("extent_test_reg2", O_CREATE | O_RDWR);
+  int regularFd3 = open("extent_test_reg3", O_CREATE | O_RDWR);
+  int regularFd4 = open("extent_test_reg4", O_CREATE | O_RDWR);
+  struct stat st;
+
+  printf(1, "In this test, we are making extent based file with really big string\n");
+  //write something in the file
+
+  printf(1, "Test1: Not that long string\n");
+  char *prompt1 = "This is not that long string!\n";
+
+  write(fd1, prompt1, strlen(prompt1));
+  write(regularFd1, prompt1, strlen(prompt1));
+  
+  printf(1, "Stat info of extent-based file using same string\n");
+  fstat(fd1, &st);
+  printStat(&st);
+
+  printf(1, "Compare to: Stat info of pointer-based file using same string\n");
+  fstat(regularFd1, &st);
+  printStat(&st);
+ 
+  printf(1, "\n\n\n");
+  printf(1, "Test2: Kind of long string\n");
+  char *prompt2 = "This is kind of long string! This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!This is kind of long string!\n";
+
+  write(fd2, prompt2, strlen(prompt2));
+  write(regularFd2, prompt2, strlen(prompt2));
+  
+  printf(1, "Stat info of extent-based file using same string\n");
+  fstat(fd2, &st);
+  printStat(&st);
+
+  printf(1, "Compare to: Stat info of pointer-based file using same string\n");
+  fstat(regularFd2, &st);
+  printStat(&st);
+ 
+  printf(1, "\n\n\n");
+  printf(1, "Test3: long string\n");
+  char *prompt3 = "This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! \n This is long string!This is long string!This is long string!This is long string!This is long string! ";
+
+  write(fd3, prompt3, strlen(prompt3));
+write(regularFd3, prompt3, strlen(prompt3));
+  
+  printf(1, "Stat info of extent-based file using same string\n");
+  fstat(fd3, &st);
+  printStat(&st);
+
+  printf(1, "Compare to: Stat info of pointer-based file using same string\n");
+  fstat(regularFd3, &st);
+  printStat(&st);
+
+
+  printf(1, "\n\n\n");
+    printf(1, "Test4: super long string\n");
+
+  char *prompt4 = "This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! This is long string! ";
+  
+  write(fd4, prompt4, strlen(prompt4));
+  write(regularFd4, prompt4, strlen(prompt4));
+  
+  printf(1, "Stat info of extent-based file using same string\n");
+  fstat(fd4, &st);
+  printStat(&st);
+
+  printf(1, "Compare to: Stat info of pointer-based file using same string\n");
+  fstat(regularFd4, &st);
+  printStat(&st);
+  
+  close(fd1);
+close(fd2);
+close(fd3);
+close(fd4);
+
+  close(regularFd1);
+close(regularFd2);
+close(regularFd3);
+close(regularFd4);
+
+exit();
 }
 
-void makeExtent(char* path)
-{
-	int fd = open(path, O_RDWR | O_CREATE | O_EXTENT);
-	char bigAssString[] = "This is a big string that just goes on and on and on aren't you jealous that you can't make it any bigger than this  massive string. I could if I had more time but I don't so instead I'll just ramble on and on. Are you even still reading this? Dont you have better things to do Please don't mark us off for this massive string.";
-	write(fd, bigAssString, sizeof(bigAssString));
-	close(fd);
-}
-
-void printStat(struct stat *statastic)
-{
-	printf(1, "Type: %d\niNode Number: %d\nNumber of Links: %d\nSize: %d bytes\n", statastic->type, statastic->ino, statastic->nlink, statastic->size);
-	if(statastic->type == T_EXTENT)
-	{
-		int n = 0;
-		while(statastic->addrs[n] && n < 13)
-		{
-			printf(1, "\tAddress: %x\n\t\tSize: %d blocks\n", getPtr(statastic->addrs[n]), getSize(statastic->addrs[n]));
-			++n;
-		}
-	}
-}
-
-uint
-toAddr(uint pointer, uint size)
-{
-  return ((pointer << 8) | size);
-}
-
-uint
-getPtr(uint addr)
-{
-  printf(1, "Getting ptr\n");
-  return ((addr & ~0xff) >> 8);
-}
-
-uint
-getSize(uint addr)
-{
-  printf(1, "Getting size\n");
-  return (addr & 0xff);
-}
